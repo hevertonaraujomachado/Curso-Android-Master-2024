f408096e4b20ccd59ae8598b4d8cee8eeadb6ec6
hevertonaraujomachado <hevertonaraujomachado@gmail.com>
2024-03-02 18:16:56 -0300

    "App Concluido"

--- AppNumber/app/src/main/java/com/devheverton/appnumber/MainActivity.java
+++ AppNumber/app/src/main/java/com/devheverton/appnumber/MainActivity.java
@@ -1,8 +1,11 @@
 package com.devheverton.appnumber;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.devheverton.appnumber.databinding.ActivityMainBinding;
@@ -32,5 +35,22 @@ public class MainActivity extends AppCompatActivity {
         }else{
             binding.txtResultado.setText("O número " +  numero  + " é Impar");
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemID = item.getItemId();
+
+        if (itemID == R.id.ic_limap){
+            binding.editNumero.setText("");
+            binding.txtResultado.setText("");
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
