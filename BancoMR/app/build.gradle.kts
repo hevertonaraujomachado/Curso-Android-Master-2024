8c3c98596b3a42d648bf1114169a1d5c97ee33da
hevertonaraujomachado <hevertonaraujomachado@gmail.com>
2024-03-07 15:00:28 -0300

    " App do Banco MR  Concuido com Sucesso!"

--- BancoMR/app/build.gradle.kts
+++ BancoMR/app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
+
+
 }
 
 android {
@@ -40,5 +43,10 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.vinaygaba:creditcardview:1.0.4")
+    implementation("com.google.firebase:firebase-bom:32.7.3")
+    implementation("com.google.firebase:firebase-database:20.3.1")
+    implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
 
 }
+
