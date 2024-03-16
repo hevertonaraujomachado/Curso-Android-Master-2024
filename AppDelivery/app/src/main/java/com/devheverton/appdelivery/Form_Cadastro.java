554adc3a26754adff900f62f764dd2338f9bbe48
hevertonaraujomachado <hevertonaraujomachado@gmail.com>
2024-03-16 18:26:05 -0300

    "Continuando o projeto parte II"

--- AppDelivery/app/src/main/java/com/devheverton/appdelivery/Form_Cadastro.java
+++ AppDelivery/app/src/main/java/com/devheverton/appdelivery/Form_Cadastro.java
@@ -1,19 +1,29 @@
 package com.devheverton.appdelivery;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.FirebaseNetworkException;
@@ -22,6 +32,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -31,6 +46,8 @@ public class Form_Cadastro extends AppCompatActivity {
     private Button bt_selecionarFoto, bt_Cadastrar;
     private EditText edit_nome, edit_email, edit_senha;
     private TextView txt_menssagemErro;
+
+    private Uri mSelecionarUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +67,15 @@ public class Form_Cadastro extends AppCompatActivity {
             }
         });
 
+        bt_selecionarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SelecionarFotoGaleria();
+
+            }
+
+        });
+
     }
 
     public void CadastrarUsuario(View view) {
@@ -61,6 +87,7 @@ public class Form_Cadastro extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
 
                 if (task.isSuccessful()) {
+                    SalvarDadosUsuario();
                     Snackbar snackbar = Snackbar.make(view, "Cadastro realizado com sucesso!", Snackbar.LENGTH_INDEFINITE)
                             .setAction("OK", new View.OnClickListener() {
                                 @Override
@@ -77,11 +104,11 @@ public class Form_Cadastro extends AppCompatActivity {
                         throw task.getException();
                     } catch (FirebaseAuthWeakPasswordException e) {
                         erro = "Coloque uma senha com no minimo 6 caracteres!";
-                    }catch (FirebaseAuthInvalidCredentialsException e){
-                     erro = "E-mail invalido!";
-                    }catch (FirebaseAuthUserCollisionException e){
+                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                        erro = "E-mail invalido!";
+                    } catch (FirebaseAuthUserCollisionException e) {
                         erro = "Esta conta já foi cadastrada!";
-                    }catch (FirebaseNetworkException e){
+                    } catch (FirebaseNetworkException e) {
                         erro = "Sem conexão com a internet!";
 
                     } catch (Exception e) {
@@ -89,47 +116,103 @@ public class Form_Cadastro extends AppCompatActivity {
                     }
                     txt_menssagemErro.setText(erro);
 
+                }
+            }
+        });
+    }
+
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        mSelecionarUri = data.getData();
+
+                        try {
+                            fotUsuario.setImageURI(mSelecionarUri);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
-            });
-        }
-        public void IniciarComponentes () {
-            fotUsuario = findViewById(R.id.fotoUsuario);
-            bt_selecionarFoto = findViewById(R.id.bt_selecionarFoto);
-            bt_Cadastrar = findViewById(R.id.bt_Cadastrar);
-            edit_nome = findViewById(R.id.edit_nome);
-            edit_email = findViewById(R.id.edit_email);
-            edit_senha = findViewById(R.id.edit_senha);
-            txt_menssagemErro = findViewById(R.id.txt_mensagemErro);
-
-
-        }
-        TextWatcher cadastroTextWatcher = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
             }
+    );
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                String nome = edit_nome.getText().toString();
-                String email = edit_email.getText().toString();
-                String semha = edit_senha.getText().toString();
-
-                if (!nome.isEmpty() && !email.isEmpty() && !semha.isEmpty()) {
-                    bt_Cadastrar.setEnabled(true);
-                    bt_Cadastrar.setBackgroundColor(getResources().getColor(R.color.dark_red));
-                } else {
-                    bt_Cadastrar.setEnabled(false);
-                    bt_Cadastrar.setBackgroundColor(getResources().getColor(R.color.gray));
-                }
-            }
+    public void SelecionarFotoGaleria() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        activityResultLauncher.launch(intent);
 
 
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-
-        };
     }
+    public void SalvarDadosUsuario(){
+
+        String nomeArquivo = UUID.randomUUID().toString();
+
+        final StorageReference reference = FirebaseStorage.getInstance().getReference("/imagens/" + nomeArquivo);
+        reference.putFile(mSelecionarUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+    @Override
+    public void onSuccess(Uri uri) {
+        Log.i("url_img", uri.toString());
+    }
+}).addOnFailureListener(new OnFailureListener() {
+    @Override
+    public void onFailure(@NonNull Exception e) {
+
+    }
+});
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+
+
+    public void IniciarComponentes() {
+        fotUsuario = findViewById(R.id.fotoUsuario);
+        bt_selecionarFoto = findViewById(R.id.bt_selecionarFoto);
+        bt_Cadastrar = findViewById(R.id.bt_Cadastrar);
+        edit_nome = findViewById(R.id.edit_nome);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        txt_menssagemErro = findViewById(R.id.txt_mensagemErro);
+
+
+    }
+
+    TextWatcher cadastroTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String nome = edit_nome.getText().toString();
+            String email = edit_email.getText().toString();
+            String semha = edit_senha.getText().toString();
+
+            if (!nome.isEmpty() && !email.isEmpty() && !semha.isEmpty()) {
+                bt_Cadastrar.setEnabled(true);
+                bt_Cadastrar.setBackgroundColor(getResources().getColor(R.color.dark_red));
+            } else {
+                bt_Cadastrar.setEnabled(false);
+                bt_Cadastrar.setBackgroundColor(getResources().getColor(R.color.gray));
+            }
+        }
+
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+
+    };
+}
