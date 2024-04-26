df57c26b45ec4cd1bdbf3c292364ca37a3e7350a
hevertonaraujomachado <hevertonaraujomachado@gmail.com>
2024-04-26 12:50:32 -0300

    App Concluindo com suceso!

--- AgendadeContatos/app/src/main/java/com/devheverton/agendadecontatos/MainActivity.kt
+++ AgendadeContatos/app/src/main/java/com/devheverton/agendadecontatos/MainActivity.kt
@@ -1,24 +1,87 @@
 package com.devheverton.agendadecontatos
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.devheverton.agendadecontatos.adapter.ContatoAdpter
+import com.devheverton.agendadecontatos.dao.UsuarioDao
 import com.devheverton.agendadecontatos.databinding.ActivityMainBinding
+import com.devheverton.agendadecontatos.model.Usuario
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var usuarioDao: UsuarioDao
+    private lateinit var contatoAdapter: ContatoAdpter
+    private val _listaUsuarios = MutableLiveData<MutableList<Usuario>>()
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        CoroutineScope(Dispatchers.IO).launch {
+            getContatos()
+
+            withContext(Dispatchers.Main){
+
+                _listaUsuarios.observe(this@MainActivity){ listaUsuarios ->
+                    val recyclerViewContatos = binding.recyclerViewContatos
+                    recyclerViewContatos.layoutManager = LinearLayoutManager(this@MainActivity)
+                    recyclerViewContatos.setHasFixedSize(true)
+                    contatoAdapter = ContatoAdpter(this@MainActivity,listaUsuarios)
+                    recyclerViewContatos.adapter = contatoAdapter
+                    contatoAdapter.notifyDataSetChanged()
+
+                }
+
+            }
+
+        }
+
         binding.btCadastrar.setOnClickListener {
             val navegarTelaCadastro = Intent(this,CadastrarUsuario::class.java)
             startActivity(navegarTelaCadastro)
         }
+
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onResume() {
+        super.onResume()
+        CoroutineScope(Dispatchers.IO).launch {
+            getContatos()
+
+            withContext(Dispatchers.Main){
+
+                _listaUsuarios.observe(this@MainActivity){ listaUsuarios ->
+                    val recyclerViewContatos = binding.recyclerViewContatos
+                    recyclerViewContatos.layoutManager = LinearLayoutManager(this@MainActivity)
+                    recyclerViewContatos.setHasFixedSize(true)
+                    contatoAdapter = ContatoAdpter(this@MainActivity,listaUsuarios)
+                    recyclerViewContatos.adapter = contatoAdapter
+                    contatoAdapter.notifyDataSetChanged()
+
+                }
+
+            }
+
+        }
+    }
+    private fun getContatos(){
+        usuarioDao = AppDatabase.getInstance(this).usuarioDao()
+        val listaUsuarios: MutableList<Usuario> = usuarioDao.get()
+        _listaUsuarios.postValue(listaUsuarios)
+
 
     }
 }
--- AgendadeContatos/app/src/main/java/com/devheverton/agendadecontatos/adapter/ContatoAdpter.kt
+++ AgendadeContatos/app/src/main/java/com/devheverton/agendadecontatos/adapter/ContatoAdpter.kt
@@ -1,15 +1,23 @@
 package com.devheverton.agendadecontatos.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.devheverton.agendadecontatos.AppDatabase
+import com.devheverton.agendadecontatos.Atualizar
+import com.devheverton.agendadecontatos.dao.UsuarioDao
 
 
 import com.devheverton.agendadecontatos.databinding.ContatoItemBinding
 import com.devheverton.agendadecontatos.model.Usuario
-
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class ContatoAdpter(private val context: Context, private val listaUsuario: MutableList<Usuario>):
@@ -25,11 +33,38 @@ class ContatoAdpter(private val context: Context, private val listaUsuario: Muta
 
 
 
+     @SuppressLint("NotifyDataSetChanged")
      override fun onBindViewHolder(holder: ContatoViewHolder, position: Int) {
         holder.txtNome.text = listaUsuario[position].nome
         holder.txtSobrenome.text = listaUsuario[position].sobrenome
         holder.txtIdade.text = listaUsuario[position].idade
         holder.txtCelular.text = listaUsuario[position].celular
+
+         holder.btAtualizar.setOnClickListener {
+             val intent = Intent(context,Atualizar::class.java)
+             intent.putExtra("nome",listaUsuario[position].nome)
+             intent.putExtra("sobrenome",listaUsuario[position].sobrenome)
+             intent.putExtra("idade",listaUsuario[position].idade)
+             intent.putExtra("celular",listaUsuario[position].celular)
+             intent.putExtra("uid",listaUsuario[position].uid)
+             context.startActivity(intent)
+
+
+         }
+         holder.btDeletar.setOnClickListener {
+             CoroutineScope(Dispatchers.IO).launch {
+                 val usuario = listaUsuario[position]
+                 val usuarioDao: UsuarioDao = AppDatabase.getInstance(context).usuarioDao()
+                 usuarioDao.deletar(usuario.uid)
+                 listaUsuario.remove(usuario)
+
+                 withContext(Dispatchers.Main){
+                     notifyDataSetChanged()
+                 }
+
+             }
+         }
+
 
      }
      override fun getItemCount() = listaUsuario.size
