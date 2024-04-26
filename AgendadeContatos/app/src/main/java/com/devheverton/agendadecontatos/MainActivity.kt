3f93c288fd379e27217806abc8a2c0bd7bcc1396
hevertonaraujomachado <hevertonaraujomachado@gmail.com>
2024-04-25 21:13:12 -0300

    concuido a segunda tela!

--- AgendadeContatos/app/src/main/java/com/devheverton/agendadecontatos/MainActivity.kt
+++ AgendadeContatos/app/src/main/java/com/devheverton/agendadecontatos/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.devheverton.agendadecontatos
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.devheverton.agendadecontatos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setContentView(R.layout.activity_main)
+        binding.btCadastrar.setOnClickListener {
+            val navegarTelaCadastro = Intent(this,CadastrarUsuario::class.java)
+            startActivity(navegarTelaCadastro)
+        }
 
     }
 }
