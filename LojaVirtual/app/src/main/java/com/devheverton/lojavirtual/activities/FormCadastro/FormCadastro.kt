0100db8d049ded31b1207f82f4b91671cddbd409
hevertonaraujomachado <hevertonaraujomachado@gmail.com>
2024-04-27 13:08:51 -0300

    Criando o layout da tela de login - parte 1

---
+++ LojaVirtual/app/src/main/java/com/devheverton/lojavirtual/activities/FormCadastro/FormCadastro.kt
@@ -0,0 +1,15 @@
+package com.devheverton.lojavirtual.activities.FormCadastro
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.devheverton.lojavirtual.R
+
+class FormCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_cadastro)
+    }
+}
---
+++ LojaVirtual/app/src/main/java/com/devheverton/lojavirtual/activities/FormLogin/FormLogin.kt
@@ -0,0 +1,17 @@
+package com.devheverton.lojavirtual.activities.FormLogin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.devheverton.lojavirtual.R
+
+class FormLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_form_login)
+
+    }
+}
