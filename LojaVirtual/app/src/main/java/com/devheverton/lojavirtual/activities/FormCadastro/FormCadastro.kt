5ad162538730cfbff45832cc2ca36f40d63aafc2
hevertonaraujomachado <hevertonaraujomachado@gmail.com>
2024-04-28 10:17:51 -0300

    Criando o layout da tela de cadastro

--- LojaVirtual/app/src/main/java/com/devheverton/lojavirtual/activities/FormCadastro/FormCadastro.kt
+++ LojaVirtual/app/src/main/java/com/devheverton/lojavirtual/activities/FormCadastro/FormCadastro.kt
@@ -6,10 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.devheverton.lojavirtual.R
+import com.devheverton.lojavirtual.databinding.ActivityFormCadastroBinding
 
 class FormCadastro : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormCadastroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_cadastro)
+        binding = ActivityFormCadastroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
     }
 }
