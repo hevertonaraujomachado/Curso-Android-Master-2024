7ec7b38adcf48820b47afbe79ccfb1c2888b3513
hevertonaraujomachado <hevertonaraujomachado@gmail.com>
2024-04-28 21:52:10 -0300

    Criando uma Dialog de carregamento

--- app/src/main/java/com/devheverton/lojavirtual/activities/FormLogin/FormLogin.kt
+++ app/src/main/java/com/devheverton/lojavirtual/activities/FormLogin/FormLogin.kt
@@ -1,6 +1,7 @@
 package com.devheverton.lojavirtual.activities.FormLogin
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,7 +9,10 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.devheverton.lojavirtual.R
 import com.devheverton.lojavirtual.activities.FormCadastro.FormCadastro
+import com.devheverton.lojavirtual.activities.TelaPrincipalProdutos.TelaPrincipalProdutos
 import com.devheverton.lojavirtual.databinding.ActivityFormLoginBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class FormLogin : AppCompatActivity() {
     private lateinit var binding: ActivityFormLoginBinding
@@ -19,10 +23,41 @@ class FormLogin : AppCompatActivity() {
 
         supportActionBar!!.hide()
 
-binding.txtTelaCadastro.setOnClickListener {
-    val intent = Intent(this,FormCadastro::class.java)
-    startActivity(intent)
+        binding.btEntrar.setOnClickListener {view ->
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
 
-}
-    }
-}
+            if (email.isEmpty() || senha.isEmpty()) {
+                val snackbar = Snackbar.make(view, "Preecha todos os campos!", Snackbar.LENGTH_LONG)
+                snackbar.setBackgroundTint(Color.RED)
+                snackbar.setTextColor(Color.WHITE)
+                snackbar.show()
+            }else{
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email,senha).addOnCompleteListener {tarefa ->
+                    if (tarefa.isSuccessful){
+                        val snackbar = Snackbar.make(view, "Sucesso ao fazer o login!", Snackbar.LENGTH_LONG)
+                        snackbar.setBackgroundTint(Color.BLUE)
+                        snackbar.setTextColor(Color.WHITE)
+                        snackbar.show()
+                        val intent = Intent(this,TelaPrincipalProdutos::class.java)
+                        startActivity(intent)
+                        finish()
+
+                    }
+
+                }.addOnFailureListener {
+                    val snackbar = Snackbar.make(view, "Erro ao fazer login", Snackbar.LENGTH_LONG)
+                    snackbar.setBackgroundTint(Color.RED)
+                    snackbar.setTextColor(Color.WHITE)
+                    snackbar.show()
+
+                }
+            }
+
+            binding.txtTelaCadastro.setOnClickListener {
+                val intent = Intent(this, FormCadastro::class.java)
+                startActivity(intent)
+
+            }
+        }
+    }}
---
+++ app/src/main/java/com/devheverton/lojavirtual/activities/TelaPrincipalProdutos/TelaPrincipalProdutos.kt
@@ -0,0 +1,16 @@
+package com.devheverton.lojavirtual.activities.TelaPrincipalProdutos
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+import com.devheverton.lojavirtual.R
+
+class TelaPrincipalProdutos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_principal_produtos)
+
+
+    }
+}
